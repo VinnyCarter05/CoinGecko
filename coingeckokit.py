c8d7c9145b5261ce46e6dc192cabffdac0c59b21
VinnyCarter05 <cartervinny05@gmail.com>
2021-12-12 16:56:00 -0500

    udpated with json files

--- coingeckokit.py
+++ coingeckokit.py
@@ -1,13 +1,14 @@
 STABLECOINS = ['ousd','usdk','rsv','musd','vai','sbd','dgd','qc','cusd','susd','eurs','usdx','gusd','xsgd','tribe','lusd','frax','husd','fei','rsr','usdn','usdp','tusd','dai','ust','busd','usdc','usdt']
 
+MODDAYS = 30 # get from website if files are older than moddays
+
 import numpy as np
 import pandas as pd
+import datetime as dt
+import json
+import os
 
 from pycoingecko import CoinGeckoAPI
-cg = CoinGeckoAPI()
-coinsList = cg.get_coins_list()
-dfCoinsList = pd.DataFrame(coinsList)
-
 
 def getCoinGeckoId(symbols, forceLower = True):
     # get CoinGeckoId from Coin symbol
@@ -31,9 +32,28 @@ def getCoinGeckoId(symbols, forceLower = True):
 def getCoinGeckoMarket (symbols, forceLower = True):
     dfMarket = pd.DataFrame()
     ids = getCoinGeckoId(symbols, forceLower)
+    direc = './data'
+    if not os.path.isdir(direc):
+        os.mkdir (direc)
     for i in range(len(ids)):
         try:
-            data = cg.get_coin_market_chart_by_id(ids[i],'usd',5000)
+            data = cg.get_coin_market_chart_by_id(ids[i],'usd',1)
+            file = './data/coingecko_coin_market_chart_by_ids_'+ids[i]+'.json'
+
+            if os.path.isfile(file):
+                modDate = os.path.getmtime(file)
+            if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)<dt.timedelta(days=modDays):
+                with open(file,'r') as f:
+                    data=json.load(f)
+#                     print ('loading')
+            else:
+                data = cg.get_coin_market_chart_by_id(ids[i],'usd',5000)
+                with open(file, 'w') as f:
+                    json.dump(data, f)
+            #         print ('new file')
+
+            
+            
             df = pd.DataFrame(data['prices'],columns=['Date','Price'])[:-1]
             df['Symbol']=symbols[i]
             df['Date']=pd.to_datetime(df['Date']*1000000)
@@ -46,5 +66,63 @@ def getCoinGeckoMarket (symbols, forceLower = True):
             print (f"Invalid symbol: {symbols[i]}")
     return dfMarket
 
+def getExchangesList (modDays = 30):
+    # get exchanges list (from website is json files is more than modDays old)
+    direc = './data'
+    file = './data/coingecko-exchanges-list.json'
+    if not os.path.isdir(direc):
+        os.mkdir (direc)
+    if os.path.isfile(file):
+        modDate = os.path.getmtime(file)
+        if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)<dt.timedelta(days=modDays):
+            with open(file,'r') as f:
+                data=json.load(f)
+#                 print ('loading')
+            return data
+    data=cg.get_exchanges_list()
+    with open(file, 'w') as f:
+        json.dump(data, f)
+#         print ('new file')
+    return data
+
+def getSupportedCurrencies (modDays = 30):
+    # get supported vs currencies list (from website is json files is more than modDays old)
+    direc = './data'
+    file = './data/coingecko-supported-vs-currencies.json'
+    if not os.path.isdir(direc):
+        os.mkdir (direc)
+    if os.path.isfile(file):
+        modDate = os.path.getmtime(file)
+        if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)<dt.timedelta(days=modDays):
+            with open(file,'r') as f:
+                data=json.load(f)
+#                 print ('loading')
+            return data
+    data=cg.get_supported_vs_currencies()
+    with open(file, 'w') as f:
+        json.dump(data, f)
+#         print ('new file')
+    return data   
     
-    
+def getCoinsList (modDays = 30):
+    # get coins list list (from website is json files is more than modDays old)
+    direc = './data'
+    file = './data/coingecko-coins-list.json'
+    if not os.path.isdir(direc):
+        os.mkdir (direc)
+    if os.path.isfile(file):
+        modDate = os.path.getmtime(file)
+        if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)<dt.timedelta(days=modDays):
+            with open(file,'r') as f:
+                data=json.load(f)
+#                 print ('loading')
+            return data
+    data=cg.get_coins_list()
+    with open(file, 'w') as f:
+        json.dump(data, f)
+#         print ('new file')
+    return data   
+
+cg = CoinGeckoAPI()
+coinsList = getCoinsList(MODDAYS)
+dfCoinsList = pd.DataFrame(coinsList)
