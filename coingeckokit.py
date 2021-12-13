2dcdeb6aa33541703d999902012667abdc70e81b
VinnyCarter05 <cartervinny05@gmail.com>
2021-12-12 23:27:13 -0500

    added .json market

--- coingeckokit.py
+++ coingeckokit.py
@@ -7,8 +7,14 @@ import pandas as pd
 import datetime as dt
 import json
 import os
+from urllib.error import HTTPError
+import time
 
 from pycoingecko import CoinGeckoAPI
+
+def FullMergeDict(one, two):
+  return { key:one.get(key,[])+two.get(key,[]) for key in set(list(one.keys())+list(two.keys())) }
+
 
 def getCoinGeckoId(symbols, forceLower = True):
     # get CoinGeckoId from Coin symbol
@@ -29,41 +35,70 @@ def getCoinGeckoId(symbols, forceLower = True):
             ids.append(np.nan)
     return ids
 
-def getCoinGeckoMarket (symbols, forceLower = True):
+def getCoinGeckoMarket (symbols, modDays = 30, forceLower = True):
     dfMarket = pd.DataFrame()
     ids = getCoinGeckoId(symbols, forceLower)
     direc = './data'
     if not os.path.isdir(direc):
         os.mkdir (direc)
+    if os.path.isfile('./data/invalid-symbols.json'):
+        with open('./data/invalid-symbols.json','r') as f:
+            invalidSymbols=json.load(f)
+    else:
+        invalidSymbols={"Invalid_Symbols":[]}
+        
     for i in range(len(ids)):
-        try:
-            data = cg.get_coin_market_chart_by_id(ids[i],'usd',1)
-            file = './data/coingecko_coin_market_chart_by_ids_'+ids[i]+'.json'
+        if symbols[i] in invalidSymbols['Invalid_Symbols']:
+            print (f'Skipping {symbols[i]}')
+            continue
+#         try:
+#             data = cg.get_coin_market_chart_by_id(ids[i],'usd',1)
+        file = './data/coingecko_coin_market_chart_by_ids_'+str(ids[i])+'.json'
 
-            if os.path.isfile(file):
-                modDate = os.path.getmtime(file)
-            if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)<dt.timedelta(days=modDays):
-                with open(file,'r') as f:
-                    data=json.load(f)
-#                     print ('loading')
-            else:
+        if os.path.isfile(file):
+            modDate = os.path.getmtime(file)
+            with open(file,'r') as f:
+                data=json.load(f)
+                print ('loading')
+            if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)>dt.timedelta(days=modDays):
+                daysOld = (dt.datetime.now()-dt.datetime.fromtimestamp(data['prices'][-1][0]/1000)).days
+                newData = cg.get_coin_market_chart_by_id(ids[i],'usd',daysOld,interval='daily')
+                data = FullMergeDict(data,newData)
+                for k, v in data.items(): # remove last item
+                    v.pop()
+                with open (file,'w') as f:
+                    json.dump(data, f)
+                    print ('appended file')
+        else:
+            try:
+                print(f"trying {symbols[i]}")
                 data = cg.get_coin_market_chart_by_id(ids[i],'usd',5000)
+                for k,v in data.items():
+                    v.pop()
                 with open(file, 'w') as f:
                     json.dump(data, f)
-            #         print ('new file')
+                    print ('new file')
+            except ValueError:
+                print (f"Invalid symbol: {symbols[i]}")
+                invalidSymbols['Invalid_Symbols'].append(symbols[i])
+                continue
 
-            
-            
-            df = pd.DataFrame(data['prices'],columns=['Date','Price'])[:-1]
-            df['Symbol']=symbols[i]
-            df['Date']=pd.to_datetime(df['Date']*1000000)
-            dfMktCap = pd.DataFrame(data['market_caps'], columns=['Date','Market_Cap'])[:-1]
-            df['Market_Cap']=dfMktCap['Market_Cap']
-            dfTotVol = pd.DataFrame(data['total_volumes'], columns=['Date','Total_Volume'])[:-1]
-            df['Total_Volume'] = dfTotVol['Total_Volume']
-            dfMarket=dfMarket.append(df,ignore_index=True)
-        except ValueError:
-            print (f"Invalid symbol: {symbols[i]}")
+
+
+        df = pd.DataFrame(data['prices'],columns=['Date','Price'])
+        df['Symbol']=symbols[i]
+        df['Date']=pd.to_datetime(df['Date']*1000000)
+        dfMktCap = pd.DataFrame(data['market_caps'], columns=['Date','Market_Cap'])
+        df['Market_Cap']=dfMktCap['Market_Cap']
+        dfTotVol = pd.DataFrame(data['total_volumes'], columns=['Date','Total_Volume'])
+        df['Total_Volume'] = dfTotVol['Total_Volume']
+        dfMarket=dfMarket.append(df,ignore_index=True)
+
+    print (invalidSymbols)
+    print (type(invalidSymbols))
+
+    with open('./data/invalid-symbols.json','w') as f:
+            json.dump(invalidSymbols,f)
     return dfMarket
 
 def getExchangesList (modDays = 30):
