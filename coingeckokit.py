09da008c728cca888be901027ef1dd52e8d5d3e8
VinnyCarter05 <74877662+VinnyCarter05@users.noreply.github.com>
2021-12-14 15:19:54 -0500

    market data for updated ids

--- coingeckokit.py
+++ coingeckokit.py
@@ -35,40 +35,40 @@ def getCoinGeckoId(symbols, forceUpper = False):
 #            print ('length: ', symbol,len(dfCoinsList[dfCoinsList['symbol']==symbol]['id']))
         if symbol in symbolId.keys():
             ids[symbol]=symbolId[symbol]
-            print (f"loading {symbol}:{ids[symbol]}")
+#             print (f"loading {symbol}:{ids[symbol]}")
             continue
         allIds = dfCoinsList[dfCoinsList['symbol']==symbol]['id']
         id = {symbol:""}
         if len(allIds)>1:
             highestMktCap = 0
             for i in range(len(allIds)):
-                print (symbol,allIds.iloc[i])
+#                 print (symbol,allIds.iloc[i])
                 try:
                     data = cg.get_coin_market_chart_by_id(allIds.iloc[i],'usd',1,interval='daily')
                     calls += 1
-                    print (calls)
+#                     print (calls)
                     if calls >= 40:
                         time.sleep(60)
                         calls = 0
                     if data['market_caps'][0][1]>highestMktCap:
-                        print (allIds.iloc[i])
+#                         print (allIds.iloc[i])
                         id[symbol] = allIds.iloc[i]
 #                         id["symbol"] = symbol
                 except ValueError:
                     print ('ValueError')
-                    print (allIds.iloc[i])
+#                     print (allIds.iloc[i])
         elif len(allIds)==1:
             try:
                 data = cg.get_coin_market_chart_by_id(allIds.iloc[0],'usd',1,interval='daily')
                 calls += 1
-                print (calls)
+#                 print (calls)
                 id[symbol] = allIds.iloc[0]
 #                 id["symbol"] = symbol
             except ValueError:
                 print (allIds.iloc[0], "doesn't exist")
         if id[symbol] != "":
             ids[symbol]=id[symbol]
-            print (symbol,':', ids[symbol])
+#             print (symbol,':', ids[symbol])
 #         except ValueError:
 #             ids.append(np.nan)
 #         except IndexError:
@@ -89,22 +89,22 @@ def getCoinGeckoMarket (symbols, modDays = 30, forceUpper = True):
     else:
         invalidSymbols={"Invalid_Symbols":[]}
         
-    for i in range(len(ids)):
-        if symbols[i] in invalidSymbols['Invalid_Symbols']:
+    for symbol in ids.keys():
+        if symbol in invalidSymbols['Invalid_Symbols']:
             print (f'Skipping {symbols[i]}')
             continue
 #         try:
 #             data = cg.get_coin_market_chart_by_id(ids[i],'usd',1)
-        file = './data/coingecko_coin_market_chart_by_ids_'+str(ids[i]["id"])+'.json'
+        file = './data/coingecko_coin_market_chart_by_ids_'+str(ids[symbol])+'.json'
 
         if os.path.isfile(file):
             modDate = os.path.getmtime(file)
             with open(file,'r') as f:
                 data=json.load(f)
-                print ('loading')
+                print ('loading', symbol)
             if dt.datetime.now() - dt.datetime.fromtimestamp(modDate)>dt.timedelta(days=modDays):
                 daysOld = (dt.datetime.now()-dt.datetime.fromtimestamp(data['prices'][-1][0]/1000)).days
-                newData = cg.get_coin_market_chart_by_id(ids[i]["id"],'usd',daysOld,interval='daily')
+                newData = cg.get_coin_market_chart_by_id(ids[symbol],'usd',daysOld,interval='daily')
                 data = FullMergeDict(data,newData)
                 for k, v in data.items(): # remove last item
                     v.pop()
@@ -113,22 +113,22 @@ def getCoinGeckoMarket (symbols, modDays = 30, forceUpper = True):
                     print ('appended file')
         else:
             try:
-                print(f"trying {symbols[i]}")
-                data = cg.get_coin_market_chart_by_id(ids[i]["id"],'usd',5000)
+                print(f"trying {symbol}")
+                data = cg.get_coin_market_chart_by_id(ids[symbol],'usd',5000)
                 for k,v in data.items():
                     v.pop()
                 with open(file, 'w') as f:
                     json.dump(data, f)
                     print ('new file')
             except ValueError:
-                print (f"Invalid symbol: {symbols[i]}")
-                invalidSymbols['Invalid_Symbols'].append(symbols[i])
+                print (f"Invalid symbol: {symbol}")
+                invalidSymbols['Invalid_Symbols'].append(symbol)
                 continue
 
 
 
         df = pd.DataFrame(data['prices'],columns=['Date','Price'])
-        df['Symbol']=symbols[i]
+        df['Symbol']=symbol
         df['Date']=pd.to_datetime(df['Date']*1000000)
         dfMktCap = pd.DataFrame(data['market_caps'], columns=['Date','Market_Cap'])
         df['Market_Cap']=dfMktCap['Market_Cap']
